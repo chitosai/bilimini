b891a7dd763b89a32aa27033eb8578add52f836a
TheC <i@thec.me>
2017-03-14 17:50:42 +0800

    视频播放页改用桌面版h5播放器，弹幕效果更好一点

--- inject.js
+++ inject.js
@@ -1,15 +1,17 @@
 window.addEventListener('DOMContentLoaded', function() {
 
 	// 自动最大化播放器
-	let playerInitCheck = setInterval(() => {
-		if( typeof window.BiliH5Player == 'function' ) {
-			var fsButton = document.querySelector('.icon-widescreen');
-			fsButton.click();
-			clearInterval(playerInitCheck);
-		} else if( ++checkCount > 100 ) {
-			clearInterval(playerInitCheck);
-		}
-	}, 50), checkCount = 0;
+	if( window.location.href.indexOf('video/av') > -1 ) {
+		let playerInitCheck = setInterval(() => {
+			let wideScreenButton;
+			if( wideScreenButton = document.querySelector('.bilibili-player-iconfont-web-fullscreen') ) {
+				wideScreenButton.click();
+				clearInterval(playerInitCheck);
+			} else if( ++checkCount > 100 ) {
+				clearInterval(playerInitCheck);
+			}
+		}, 50), checkCount = 0;
+	}
 
 	// 移除app广告
 	let ad = document.getElementById('b_app_link');
