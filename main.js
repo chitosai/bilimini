2ad083d3b5550c6e5332aa463f066ef6bd5bb7f5
TheC <i@thec.me>
2017-03-16 18:17:45 +0800

    老板键改为alt+p，这样win和mac下应该能通用吧

--- main.js
+++ main.js
@@ -50,8 +50,7 @@ app.on('activate', () => {
 
 // 老板键
 function bindGloablShortcut() {
-  let hotkey = platform == 'win' ? 'Ctrl+p' : 'Command+p';
-  shortcut.register(hotkey, () => {
+  shortcut.register('alt+p', () => {
     if( !mw ) return false;
     if( mw.isVisible() ) {
       mw.hide();
