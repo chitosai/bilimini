c963bdf61e66c9974851e3ac2038382e15411fcc
TheC <i@thec.me>
2017-05-22 14:16:41 +0800

    允许localstorage储存任何类型数据

--- js/index.js
+++ js/index.js
@@ -16,15 +16,19 @@ let wv, wrapper;
 // 保存用户设置
 var Config = {
     load: function(key) {
-        return localStorage.getItem(key);
+        var value = JSON.parse(localStorage.getItem(key));
+        if( value != null ) {
+            Config[key] = value;
+        }
+        return value;
     },
     set: function(key, value) {
         if( typeof key == 'object' ) {
             for( let _k of key ) {
-                window.localStorage.setItem(_k, key[_k]);
+                window.localStorage.setItem(_k, JSON.stringify(key[_k]));
             }
         } else if( typeof key == 'string' && value ) {
-            window.localStorage.setItem(key, value);
+            window.localStorage.setItem(key, JSON.stringify(value));
         }
     },
     'windowSizeMini': [300, 187],
@@ -34,10 +38,7 @@ var Config = {
 // 初始化时读取用户设置
 function loadUserConfig() {
     // 读取用户拖的视频播放窗口尺寸
-    var windowSizeMini = Config.load('windowSizeMini');
-    if( windowSizeMini ) {
-        Config.windowSizeMini = windowSizeMini;
-    }
+    Config.load('windowSizeMini');
 }
 
 // 保存用户浏览记录
