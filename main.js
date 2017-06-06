6bb07f897c4123d207fee07d5a9d78d19f638542
TheC <i@thec.me>
2017-06-06 16:39:18 +0800

    不再由window-all-closed事件触发quit

--- main.js
+++ main.js
@@ -147,15 +147,6 @@ function init() {
 // Some APIs can only be used after this event occurs.
 app.on('ready', init);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if( process.platform !== 'darwin' ) {
-    app.quit();
-  }
-});
-
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
