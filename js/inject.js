0d7aceaa88500396ef26fb7df34bd9dc0ddd4c07
TheC <i@thec.me>
2017-09-04 10:58:22 +0800

    h5播放器好像更新了，全屏后出现了横向滚动条，隐藏掉

--- js/inject.js
+++ js/inject.js
@@ -16,6 +16,8 @@ window.addEventListener('DOMContentLoaded', function() {
       let wideScreenButton;
       if( wideScreenButton = document.querySelector('.bilibili-player-iconfont-web-fullscreen') ) {
         wideScreenButton.click();
+        // 隐藏全屏播放器（在某些情况下会出现）的滚动条
+        document.body.style.overflow = 'hidden';
         clearInterval(playerInitCheck);
       } else if( ++checkCount > 100 ) {
         clearInterval(playerInitCheck);
