245e17d4136e53f7bd36dcdcd10ddb7e946a264c
TheC <i@thec.me>
2018-02-22 18:03:50 +0800

    b站增加一种可播放url格式：bangumi/play/

--- js/inject.js
+++ js/inject.js
@@ -13,7 +13,9 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   // 普通视频页：自动最大化播放器
-  if( window.location.href.indexOf('video/av') > -1 || window.location.href.indexOf('html5player.html') > -1 ) {
+  if( window.location.href.indexOf('video/av') > -1 || 
+      window.location.href.indexOf('html5player.html') > -1 ||
+      window.location.href.indexOf('bangumi/play/') > -1 ) {
     let playerInitCheck = setInterval(() => {
       let wideScreenButton;
       if( wideScreenButton = document.querySelector('.bilibili-player-iconfont-web-fullscreen') ) {
